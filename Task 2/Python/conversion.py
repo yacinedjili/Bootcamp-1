05d465ddbd8e919e034e40e1e9acc8b13f21317f
yacinedjili <yacinedjili@outlook.com>
2024-10-18 22:04:59 +0100

    conversion.py

---
+++ Task 2/Python/conversion.py
@@ -0,0 +1,23 @@
+# Step 1: Declare variables
+num1 = 99.23
+num2 = 23
+num3 = 150
+string1 = "100"
+
+# Step 2: Convert num1 (float) into an integer
+num1_converted = int(num1)
+
+# Step 3: Convert num2 (integer) into a float
+num2_converted = float(num2)
+
+# Step 4: Convert num3 (integer) into a string
+num3_converted = str(num3)
+
+# Step 5: Convert string1 (string) into an integer
+string1_converted = int(string1)
+
+# Step 6: Print all the converted variables on separate lines
+print(num1_converted)    # Print num1 as integer
+print(num2_converted)    # Print num2 as float
+print(num3_converted)    # Print num3 as string
+print(string1_converted) # Print string1 as integer
