44b28dd08402ac4652d6541e1a48de4b660aa49f
yacinedjili <yacinedjili@outlook.com>
2024-10-18 22:02:31 +0100

    hello_world.py

---
+++ Task 2/Python/hello_world.py
@@ -0,0 +1,14 @@
+# Step 1: Prompt the user for their name
+name = input("Please enter your name: ")
+
+# Step 2: Print the user's name
+print("Your name is:", name)
+
+# Step 3: Prompt the user for their age
+age = input("Please enter your age: ")
+
+# Step 4: Print the user's age
+print("Your age is:", age)
+
+# Step 5: Print "Hello World." on a new line
+print("Hello World.")
